0739cb82b710b2422f779652fe22da05f6144cb1
whathebea <bea.graciast@gmail.com>
2023-01-13 21:49:01 -0300

    Upload Service

---
+++ src/main/java/com/whathebea/fileupload/fileupload/FileuploadApplication.java
@@ -0,0 +1,25 @@
+package com.whathebea.fileupload.fileupload;
+
+import com.whathebea.fileupload.fileupload.service.StorageService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
+public class FileuploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileuploadApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+	}
+}
